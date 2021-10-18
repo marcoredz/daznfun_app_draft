f312d97e0342296241788d63a667e7bcd4c5d779
Marco Rossi <marcoreds9724@gmail.com>
2021-10-18 23:43:29 +0200

    login page layout

--- lib/apptheme.dart
+++ lib/apptheme.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-/* DAZN color pattern
-  Login BG color: #242d34
-
-*/
+/* DAZN Color Pattern */
+class DaznCP {
+  static const Color loginBgColor = Color(0xFF242d34);
+  static const Color loginGray = Color(0xFF666C71);
+  static const Color accent = Color(0xFFf7ff1a);
+  static const Color secondary = Color(0xFF0c161c);
+}
 
 class DaznTheme {
   static TextTheme textTheme = const TextTheme(
     bodyText1: TextStyle(
+      fontFamily: 'DaznTrim',
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.white,
+    ),
+    bodyText2: TextStyle(
       fontFamily: 'DaznTrim',
       fontSize: 14.0,
       fontWeight: FontWeight.w400,
@@ -53,7 +63,39 @@ class DaznTheme {
       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
         selectedItemColor: Colors.green,
       ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(DaznCP.accent),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          shape: MaterialStateProperty.all<BeveledRectangleBorder>(
+              BeveledRectangleBorder(borderRadius: BorderRadius.circular(0))),
+          padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(12)),
+          textStyle: MaterialStateProperty.all<TextStyle>(textTheme.headline3 ?? const TextStyle()),
+        ),
+      ),
       textTheme: textTheme,
+      textSelectionTheme: const TextSelectionThemeData(
+        cursorColor: Colors.white,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: Colors.white),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: DaznCP.loginGray),
+          borderRadius: BorderRadius.zero,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: Colors.black),
+          borderRadius: BorderRadius.zero,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: Colors.red),
+          borderRadius: BorderRadius.zero,
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 1, color: Colors.red),
+          borderRadius: BorderRadius.zero,
+        ),
+      ),
     );
   }
 }
